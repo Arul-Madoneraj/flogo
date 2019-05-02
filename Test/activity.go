39acaedfaae937274689e277cd5d5311eb49ad3d
Arul-Madoneraj <50017615+Arul-Madoneraj@users.noreply.github.com>
2019-05-02 16:09:09 +0200

    Add files via upload

---
+++ Test/activity.go
@@ -0,0 +1,37 @@
+package Test
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+	var log = logger.GetLogger("activity-helloworld")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+
+	// do eval
+	firstname := context.GetInput("firstname").(string)
+	lastname := context.GetInput("lastname").(string)
+	
+	log.Infof("Received Firstname:[%s] and lastname [%s]",firstname,lastname)
+	
+	context.SetOutput("result",lastname+","+firstname)
+
+	return true, nil
+}
